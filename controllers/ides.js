299ab3cf8330053d71c4cdc75a47bde96acbeae6
Fatih Kaya <1994274@gmail.com>
2016-02-20 19:07:07 +0000

    Accept http username and password correctly.

--- controllers/ides.js
+++ controllers/ides.js
@@ -47,9 +47,9 @@ function add(req, res) {
     return res.send('name is required.');
   if (typeof req.body.dir !== 'string' || req.body.dir.length === 0)
     return res.send('dir is required.');
-  if (typeof req.body.user !== 'string' && req.body.user.length > 0)
+  if (typeof req.body.user === 'string' && req.body.user.length > 0)
     user = req.body.user;
-  if (typeof req.body.pass !== 'string' && req.body.pass.length > 0)
+  if (typeof req.body.pass === 'string' && req.body.pass.length > 0)
     pass = req.body.pass;
 
   if (typeof req.body.port === 'string' && req.body.port.length > 0) {
