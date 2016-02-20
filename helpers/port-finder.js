ac5ac6861d38e0fa330dcf9b0b8ea873a7082925
Fatih Kaya <1994274@gmail.com>
2016-02-20 11:45:03 +0000

    Add documentation for port finder helper.

--- helpers/port-finder.js
+++ helpers/port-finder.js
@@ -2,6 +2,12 @@ var Promise = require("bluebird");
 var config = require('../config');
 var check = require('./port-checker');
 
+/**
+ * Find first available port
+ */
+
+// internal function that checks port with port checker helper
+// and recursively calls find function until find avaliable port
 function findGeneral(port, cb) {
   check(port)
     .then(res => {
@@ -12,6 +18,15 @@ function findGeneral(port, cb) {
     })
     .catch(err => cb (err));
 }
+
+/**
+ * Find first available port after @param port.
+ * @param {number} [port=config.startPort] Start port
+ * @param {function=} cb Callback function. If not set, this function
+ *  returns a Promise
+ * @returns {undefined|Promise} If cb is set, returns undefined,
+ *  otherwise returns a Promise.
+ */
 
 function find(port, cb) {
   var startPort = config.startPort;
